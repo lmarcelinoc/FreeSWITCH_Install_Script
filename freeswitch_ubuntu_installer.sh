88c9a18d97477edae5d8ec4f6aef2209b11a2a09
teknoprep <chris@blueuc.com>
2023-12-06 06:08:06 -0500

    Update freeswitch_ubuntu_installer.sh

--- freeswitch_ubuntu_installer.sh
+++ freeswitch_ubuntu_installer.sh
@@ -1,9 +1,11 @@
-version=1.10.7
+version=1.10.10.
 
 apt -y update
 apt -y install sudo
-sudo apt install -y git subversion build-essential autoconf automake libtool libncurses5 libncurses5-dev make libjpeg-dev libtool libtool-bin libsqlite3-dev libpcre3-dev libspeexdsp-dev libldns-dev libedit-dev yasm liblua5.2-dev libopus-dev cmake libcurl4-openssl-dev libexpat1-dev libgnutls28-dev libtiff5-dev libx11-dev unixodbc-dev libssl-dev python-dev zlib1g-dev libasound2-dev libogg-dev libvorbis-dev libperl-dev libgdbm-dev libdb-dev uuid-dev libsndfile1-dev unzip python3.8-distutils
+sudo apt install -y git subversion build-essential autoconf automake libtool libncurses5 libncurses5-dev make libjpeg-dev libtool libtool-bin libsqlite3-dev libpcre3-dev libspeexdsp-dev libldns-dev libedit-dev yasm liblua5.2-dev libopus-dev cmake libcurl4-openssl-dev libexpat1-dev libgnutls28-dev libtiff5-dev libx11-dev unixodbc-dev libssl-dev python3.11-dev zlib1g-dev libasound2-dev libogg-dev libvorbis-dev libperl-dev libgdbm-dev libdb-dev uuid-dev libsndfile1-dev unzip python3.11-distutils
 sudo apt install -y libavformat-dev libswscale-dev libpq-dev libshout3-dev
+
+sudo apt -y install unzip
 
 cd /usr/src
 sudo git clone https://github.com/signalwire/libks.git
@@ -20,16 +22,19 @@ sudo make
 sudo make install
 
 cd /usr/src
-sudo wget https://files.freeswitch.org/freeswitch-releases/freeswitch-$version.-release.zip
-sudo apt -y install 
-sudo unzip freeswitch-$version.-release.zip
-cd freeswitch-$version.-release/
+rm freeswitch*zip*
+sudo wget https://files.freeswitch.org/freeswitch-releases/freeswitch-$version-release.zip
+sudo unzip freeswitch-$version-release.zip
 
-sudo apt -y install unzip
+sleep 10
 
+
+# Remove existing symlink if it exists
+sudo rm -f /usr/src/freeswitch
+
+# Create new symlink
 cd /usr/src
-ln -s /usr/src/freeswitch-$version.-release/ /usr/src/freeswitch
-
+sudo ln -s /usr/src/freeswitch-${version}-release/ /usr/src/freeswitch
 
 cd /usr/src/freeswitch
 git clone https://github.com/freeswitch/spandsp.git
@@ -65,7 +70,7 @@ sudo chown -R freeswitch:freeswitch /usr/local/freeswitch/
 sudo chmod -R ug=rwX,o= /usr/local/freeswitch/
 sudo chmod -R u=rwx,g=rx /usr/local/freeswitch/bin/
 
-
+rm /etc/systemd/system/freeswitch.service
 sudo tee -a /etc/systemd/system/freeswitch.service  <<EOF
 [Unit]
 Description=freeswitch
